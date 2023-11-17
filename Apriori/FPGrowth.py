9421fcbdcb3c5ee84343dedef35a74804414a9a5
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-17 18:36:33 +0700

    Add FP-Growth implementation

---
+++ Apriori/FPGrowth.py
@@ -0,0 +1,10 @@
+transactions = [
+    ["Hot Dogs", "Buns", "Ketchup"],
+    ["Hot Dogs", "Buns"],
+    ["Hot Dogs", "Coke", "Chips"],
+    ["Chips", "Coke"],
+    ["Chips", "Ketchup"],
+    ["Hot Dogs", "Coke", "Chips"]
+]
+
+# FP-Growth
